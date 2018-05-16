1b16f04b2b1e8983d1339b3b6d7012a84468db16
BillyNgu <billy.ngn@eduge.ch>
2018-05-16 10:36:45 +0200

    .

--- play.php
+++ play.php
@@ -1,17 +1,21 @@
+<?php
+
+require_once './dao/dao.php';
+
+$nickname = $_SESSION['pseudo'];
+$play = TRUE;
+
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8"> 
+        <title>VidéoQuizz</title>
+        <?php require_once './css/css_js.php'; ?>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <?php require_once './navbar.php'; ?>
+        </div>
     </body>
 </html>
